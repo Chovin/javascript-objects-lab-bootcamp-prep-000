1a7b4a1f865295c26a8885f8844bd4cea88f5ac8
Chovin Carlson <chovin@guam.net>
2018-08-25 01:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object,key) { return Object.assign({},object,{}); }
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+
